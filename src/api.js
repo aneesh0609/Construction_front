8ac0673fb31514ce7a6c85f1b9881e23a060ac11
aneesh0609 <aneeshchauhan0609@gmail.com>
2025-11-12 21:33:50 +0530

    new changes

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://construction-back.onrender.com/api",
   withCredentials: true,
 });
 
